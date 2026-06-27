48e0380e7fad3290f1fb8bfac69fcffe88b003a6
Amit Suryawanshi <lamerix.sales@gmail.com>
2026-06-27 11:49:07 +0530

    Fix model-viewer TypeScript declaration

--- model-viewer.d.ts
+++ model-viewer.d.ts
@@ -1,7 +1,26 @@
+import React from "react";
+
 declare global {
   namespace JSX {
     interface IntrinsicElements {
-      "model-viewer": any;
+      "model-viewer": React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      > & {
+        src?: string;
+        alt?: string;
+        poster?: string;
+        loading?: "auto" | "lazy" | "eager";
+        reveal?: "auto" | "interaction";
+        "camera-controls"?: boolean;
+        "auto-rotate"?: boolean;
+        "shadow-intensity"?: string;
+        exposure?: string;
+        "environment-image"?: string;
+        ar?: boolean;
+        "ar-modes"?: string;
+        "ios-src"?: string;
+      };
     }
   }
 }
